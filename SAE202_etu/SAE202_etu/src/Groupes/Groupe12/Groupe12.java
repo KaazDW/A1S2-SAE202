4475c0c13bd77c82376d2ecb9cb9063a5d705c13
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-06-08 10:08:00 +0200

    Add files via upload

---
+++ SAE202_etu/SAE202_etu/src/Groupes/Groupe12/Groupe12.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Groupes.Groupe12;
+
+import packMesClassesEtInterfaces.SAE202_Interface;
+
+
+/**
+ *
+ * 
+ */
+public class Groupe12 implements SAE202_Interface{
+
+    @Override
+    public void challenge(String prefixeFichier, Integer nbFichiers, Long millisecondes) {
+        
+    }
+
+    @Override
+    public void modelisation(String prefixeFichier, Integer nbFichiers, Long millisecondes) {
+        
+    }
+
+    
+    
+   
+}
---
+++ SAE202_etu/SAE202_etu/src/sae202_etu/SAE202_etu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sae202_etu;
+
+/**
+ *
+ * @author brice.effantin
+ */
+public class SAE202_etu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
